58732b67a9e916629bdfd09a7d976b044f1a6f6c
Ezequiel Beuter <ezequielbeuter@gmail.com>
2024-10-31 17:41:08 -0300

    Modificacion de las rutas AUTH

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,12 @@
         <nav>
             <a href="{{route('home')}}" class="nav-link">home</a>
             @auth
+            <a href="{{ route('clientes.index') }}" class="nav-link">Clientes</a>
+            <a href="{{ route('tecnicos.index') }}" class="nav-link">Técnicos</a>
+            <a href="{{ route('equipos.index') }}" class="nav-link">Equipos</a>
+            <a href="{{ route('equiposdetrabajo.index') }}" class="nav-link">Equipos de Trabajo</a>
+            <a href="{{ route('vehiculos.index') }}" class="nav-link">Vehículos</a>
+            <a href="{{ route('ordenes.index') }}" class="nav-link">Órdenes</a>
             <form action="{{route('logout')}}" method="post">
                 @csrf
                 <button class="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Logout</button>
--- routes/web.php
+++ routes/web.php
@@ -7,35 +7,30 @@ use App\Http\Controllers\EquiposDeTrabajoController;
 use App\Http\Controllers\OrdenController;
 use App\Http\Controllers\TecnicoController;
 use App\Http\Controllers\VehiculoController;
-use App\Models\Cliente;
-use App\Models\Equipo;
-use App\Models\EquiposDeTrabajo;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','posts.index')->name('home');
-Route::resource('vehiculos', VehiculoController::class);
-Route::resource('ordenes', OrdenController::class);
-Route::resource('equipos', EquipoController::class);
-Route::resource('equiposdetrabajo', EquiposDeTrabajoController::class);
-Route::resource('clientes', ClienteController::class);
-Route::get('/clientes/inactivos',
-[ClienteController::class, 'inactivos'])->name('clientes.inactivos');
 
-Route::post('/clientes/{cliente}/rehabilitar', [ClienteController::class, 'up'])->name('clientes.rehabilitar');
-
-Route::resource('tecnicos', TecnicoController::class);
-
-Route::middleware('guest')->group(function(){
-
-    Route::view('/register','auth.register')->name('register');
+Route::view('/', 'posts.index')->name('home');
+Route::middleware('guest')->group(function() {
+    Route::view('/register', 'auth.register')->name('register');
     Route::post('/register', [AuthController::class, 'register']);
-
-    Route::view('/login','auth.login')->name('login');
+    
+    Route::view('/login', 'auth.login')->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 });
 
-Route::middleware('auth')->group(function(){
-    Route::post('/logout',[AuthController::class, 'logout'])->name('logout');
 
+Route::middleware('auth')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    Route::resource('vehiculos', VehiculoController::class);
+    Route::resource('ordenes', OrdenController::class);
+    Route::resource('equipos', EquipoController::class);
+    Route::resource('equiposdetrabajo', EquiposDeTrabajoController::class);
+    Route::resource('clientes', ClienteController::class);
+    
+    Route::get('/clientes/inactivos', [ClienteController::class, 'inactivos'])->name('clientes.inactivos');
+    Route::post('/clientes/{cliente}/rehabilitar', [ClienteController::class, 'up'])->name('clientes.rehabilitar');
+    
+    Route::resource('tecnicos', TecnicoController::class);
 });
-
